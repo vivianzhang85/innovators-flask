d297a7091be6d2a36845ec73f6df64a113b240a8
jm1021 <jmort1021@gmail.com>
2026-01-13 03:42:17 -0800

    port updates

--- socket/socket_server.py
+++ socket/socket_server.py
@@ -49,4 +49,4 @@ def handle_get_leaderboard():
 # this runs the flask application on the development server
 if __name__ == "__main__":
     # change name for testing
-    socketio.run(app, debug=True, host="0.0.0.0", port=8500)
+    socketio.run(app, debug=True, host="0.0.0.0", port=8303)
